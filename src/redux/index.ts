dad34cd6c85cd767c8b22c8632d0061b8d584c49
Luiz Gabriel <lbrebelatto@gmail.com>
2023-05-13 20:06:15 -0300

    feat: reducers and types

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,8 +1,25 @@
-const BUY_CAKE = 'BUY_CAKE';
+import { IAction, EnumAction } from "./types";
+
 
 function buyCake(){
     return {
-        type: BUY_CAKE,
+        type: EnumAction.BUY_CAKE,
         info: 'First redux action',
+    }
+};
+
+const initialState = {
+    numOfCakes: 10
+};
+
+const reducer = (state = initialState, action: IAction) => {
+    switch(action.type) {
+        case EnumAction.BUY_CAKE: return {
+            // esse spread operator esta aqui para criar uma copia dos dados
+            ...state, 
+            numOfCakes: state.numOfCakes - 1 
+        }
+         
+        default: return state;
     }
 }
---
+++ src/redux/types.ts
@@ -0,0 +1,5 @@
+export type IAction = { type: "BUY_CAKE" };
+
+export enum EnumAction {
+    BUY_CAKE = "BUY_CAKE"
+}
